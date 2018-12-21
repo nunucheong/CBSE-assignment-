abf5825f01f5dadbf451c77f4650f761f5b9552f
nunucheong <nunucheong@gmail.com>
2018-12-21 13:52:39 +0800

    Final touches for presentation

--- PalindromeWeb/src/java/com/cbse/example/tester/PalindromeController.java
+++ PalindromeWeb/src/java/com/cbse/example/tester/PalindromeController.java
@@ -88,7 +88,83 @@ public class PalindromeController extends HttpServlet {
             } else {
                 responseString = "The String is not palindromic!";
             }
-            out.println(responseString);
+            String htmlResponse = "<html>\n" +
+"    <head>\n" +
+"        <title>Palindrome Check</title>\n" +
+"        <meta charset=\"UTF-8\">\n" +
+"        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+"        <style type=\"text/css\">\n" +
+"            <!--\n" +
+"               body {\n"+
+                    "background-color: #FFA500;\n"+
+                    "}\n"+
+"        	h1{\n" +
+"        		color: white;\n" +
+"        		margin: 0 auto;\n" +
+"        		text-align: center;\n" +
+"        		font-size: 90px;\n" +
+"        		font-style: bold;\n" +
+"        		font-family: Brush Script MT, Brush Script Std, cursive;\n" +
+"        	}\n" +
+"        	body {\n" +
+"	 			 background-image: url(\"orange.jpg\");\n" +
+"	 			 background-repeat: no-repeat;\n" +
+"	 			 background-position: right top;\n" +
+"	 			 background-size: cover;\n" +
+"	 			 padding-top: 40px;\n" +
+"  				 padding-right: 30px;\n" +
+"  				 padding-bottom: 50px;\n" +
+"  				 padding-left: 80px;\n" +
+"			}\n" +
+"			h2 {\n" +
+"        		color: white;\n" +
+"				text-align: center;\n" +
+"                       font-size: 60px;\n" +                    
+"	    		width: 500px;\n" +
+"	    		margin: 0 auto;\n" +
+"			}\n" +
+                    "form {\n" +
+				"text-align: center;\n" +
+	    		"width: 500px; \n" +
+	    		"margin: 0 auto; \n" +
+			"}\n" +
+"			input[type=submit] {\n" +
+"  			background-color: #ff0066;\n" +
+"  			color: white;\n" +
+"  			padding: 12px 20px;\n" +
+"  			border: none;\n" +
+"  			border-radius: 4px;\n" +
+"  			cursor: pointer;\n" +
+"  			text-align: center;\n" +
+"                        }\n" +
+"\n" +
+"			input[type=submit]:hover {\n" +
+" 				 background-color: #d1a77b;\n" +
+"			}\n" +
+"			.footer {\n" +
+"   				position: fixed;\n" +
+"			   left: 0;\n" +
+"			   bottom: 0;\n" +
+"			   width: 100%;\n" +
+"			   color: white;\n" +
+"			   text-align: left;\n" +
+"			   padding-left: 15px;\n" +
+"			}\n" +
+"                        -->\n" +
+"        </style>\n" +
+"    </head>\n" +
+"    <body>\n" +
+"    	<h1>Palindrome Checker</h1><br><br><br><br><br><br><br>\n" +
+                    "<h2>" + responseString+ "</h2><br>\n"+
+"        <form action=\"http://localhost:8080/PalindromeWeb/\" method=\"post\">\n" +
+"            <input type=\"submit\" value=\"Back\" />\n" +
+"        </form>\n" +
+"        <div class=\"footer\">\n" +
+"  			<p>This web application is for academic purpose only.</p>\n" +
+"		</div>\n" +
+"    </body>\n" +
+"</html>";
+            out.println(htmlResponse);
         }
     }
 
--- PalindromicClient/build/generated-sources/ap-source-output/com/cbse/example/entity/Palindrome_.java
+++ PalindromicClient/build/generated-sources/ap-source-output/com/cbse/example/entity/Palindrome_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-26T12:05:00")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-17T23:47:17")
 @StaticMetamodel(Palindrome.class)
 public class Palindrome_ { 
 
